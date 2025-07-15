10467a3db9d7f09d947289e76e0c457cea64a450
Roy <156922911+ArunRoy404@users.noreply.github.com>
2025-07-16 04:03:14 +0600

    reat hot toast installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 
 import ProviderContainer from "./providers/ProviderContainer.jsx";
 import { RouterProvider } from "react-router";
 import router from "./routes/router.jsx";
+import { Toaster } from "react-hot-toast";
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
       <ProviderContainer>
+        <Toaster></Toaster>
         <RouterProvider router={router}/>
       </ProviderContainer>
   </StrictMode>
