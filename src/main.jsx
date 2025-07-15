3571b85438b9ac734359d050f33ab355a58f6a28
Roy <156922911+ArunRoy404@users.noreply.github.com>
2025-07-16 03:21:08 +0600

    ThemeProvider added

--- src/main.jsx
+++ src/main.jsx
@@ -2,41 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@material-tailwind/react";
-import AuthProvider from "./providers/AuthProvider.jsx";
+
 import ProviderContainer from "./providers/ProviderContainer.jsx";
-
-
-const customTheme = {
-  button: {
-    defaultProps: {
-      color: "primary",
-      variant: "filled",
-    },
-    styles: {
-      base: {
-        initial: {
-          fontWeight: "font-medium",
-          borderRadius: "rounded-lg",
-        },
-      },
-    },
-  },
-  input: {
-    defaultProps: {
-      color: "primary",
-      variant: "outlined",
-    },
-  },
-};
-
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider value={customTheme}>
       <ProviderContainer>
         <App />
       </ProviderContainer>
-    </ThemeProvider>
   </StrictMode>
 );
--- src/providers/ProviderContainer.jsx
+++ src/providers/ProviderContainer.jsx
@@ -1,9 +1,37 @@
+import { ThemeProvider } from '@material-tailwind/react';
 import AuthProvider from './AuthProvider';
+
+const customTheme = {
+    button: {
+        defaultProps: {
+            color: "primary",
+            variant: "filled",
+        },
+        styles: {
+            base: {
+                initial: {
+                    fontWeight: "font-medium",
+                    borderRadius: "rounded-lg",
+                },
+            },
+        },
+    },
+    input: {
+        defaultProps: {
+            color: "primary",
+            variant: "outlined",
+        },
+    },
+};
+
+
 
 const ProviderContainer = ({ children }) => {
     return (
         <AuthProvider>
-            {children}
+            <ThemeProvider value={customTheme}>
+                {children}
+            </ThemeProvider>
         </AuthProvider>
     );
 };
