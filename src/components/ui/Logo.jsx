2e4bc6f1c52755f8a8661a24a239007e2c07ded0
Roy <156922911+ArunRoy404@users.noreply.github.com>
2025-07-17 20:45:12 +0600

    logo for navbar added

---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,20 @@
+import logoDark from '/logo/PetLink-Dark.png'
+import logoLight from '/logo/PetLink-Light.png'
+import { useThemeContext } from '../../context/ThemeContext';
+import { Link } from 'react-router';
+
+
+const Logo = () => {
+    const { isDark } = useThemeContext()
+
+    return (
+        <Link to='/'>
+            <div className='flex items-center gap-4 cursor-pointer'>
+                <img className='w-8' src={isDark ? logoLight : logoDark} alt="" />
+                <h1 className='text-2xl font-bold'>PetLink</h1>
+            </div>
+        </Link>
+    );
+};
+
+export default Logo;
